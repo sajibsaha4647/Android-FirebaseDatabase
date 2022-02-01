0c50fe7010bd7a3dcd386f8513aea3f5d8db82f3
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-02-01 11:57:20 +0600

    updated

--- app/src/main/java/com/example/firebasedatabase/Activities/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.firebasedatabase;
+package com.example.firebasedatabase.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,24 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.firebasedatabase.Customclass.Student;
+import com.example.firebasedatabase.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText nametext,emailtext;
     private Button savebtn;
 
-
-//    int width = (int) (getResources().getDisplayMetrics().widthPixels * 0.85);
-//    int height = (int) (getResources().getDisplayMetrics().heightPixels * 0.85);
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databaseReference = FirebaseDatabase.getInstance().getReference("students");
 
         nametext = findViewById(R.id.idname);
         emailtext = findViewById(R.id.idemail);
         savebtn = findViewById(R.id.idSave);
+
 //        savebtn.getLayoutParams().width = (int) (getResources().getDisplayMetrics().widthPixels * 0.09);
         savebtn.getLayoutParams().height = (int) (getResources().getDisplayMetrics().heightPixels * 0.1);
 
@@ -40,6 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String name = nametext.getText().toString();
         String email = emailtext.getText().toString();
 
+        String key = databaseReference.push().getKey();
+
+        Student student = new Student(name,email);
+
+        databaseReference.child(key).setValue(student);
+
+        Toast.makeText(getApplicationContext(),"Data save successful",Toast.LENGTH_LONG).show();
 
     }
 }
